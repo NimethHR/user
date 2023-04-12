f08621239ad771ded33dce350d95f8b96c446f1d
Nimeth <nrh.herath@gmail.com>
2023-04-12 15:46:19 +0530

    3

---
+++ src/components/profile/Prdetails.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+const Prdetails = () => {
+    const [details, setDetails]=useState(initialState)
+
+    //hook to fetch data
+        // useEffect { ()=>{
+
+        // }
+
+
+        // },
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Prdetails
